bdb79f20906503e31a540569e78cb373beba5697
saharnazb <saharnaz.babaei@grad.moore.sc.edu>
2019-09-30 21:11:32 -0400

    PS3

--- ProblemSets/PS3/PS3_Babaei.py
+++ ProblemSets/PS3/PS3_Babaei.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-# Saharnaz Babaei Balderlou 
+# Saharnaz Babaei Balderlou
 # Problem Set 3
 #------------------------------------------------------------------------------
 # Import required packages
@@ -16,30 +10,17 @@ import matplotlib.pyplot as plt # will be used to see the obvious relationship o
 import math # to use in some equations
 from scipy.optimize import minimize # for optimization of Likelihood function (MLE method)
 import scipy.optimize as opt
-import statsmodels.api as statmod 
+import statsmodels.api as statmod
 import scipy.stats as stats
-
-
-# In[2]:
-
 
 #------------------------------------------------------------------------------
 # Read data and prepare to utilize (Part 1 & 2)
 #------------------------------------------------------------------------------
-df1 = pd.read_stata('PS3_data.dta') 
+df1 = pd.read_stata('PS3_data.dta')
 # My note for later: https://www.shanelynn.ie/using-pandas-dataframe-creating-editing-viewing-data-in-python/
-
-
-# In[3]:
-
 
 print("Dataframe: ")
 df1.head()
-
-
-# In[4]:
-
-
 '''
 hlabinc = annual labor income of the head
 hannhrs = annual hours of the head
@@ -49,30 +30,15 @@ age = age of the head
 hyrsed = years of education of the head
 '''
 print("Data Statistics:")
-df1.describe() 
-
-
-# In[5]:
-
-
+df1.describe()
 print("Scatterplot between annual labor inome of the head and years of education of the head")
 plt.style.use('seaborn')
 df1.plot(x = 'hyrsed', y = 'hlabinc', kind = 'scatter')
 plt.show()
 plt.savefig('scat_inc_edu.png')
-
-
-# In[6]:
-
-
 #drop missing values
 df1_subset = df1.dropna(how = 'any', subset = ['hlabinc', 'hannhrs', 'hsex', 'hrace', 'age', 'hyrsed'])
 df1_subset.describe()
-
-
-# In[7]:
-
-
 # select male heads of HH whose age is between 25 & 60 (included!), and wage > $7/hr
 df2 = df1_subset[['id68', 'year', 'intid', 'hlabinc', 'hannhrs', 'hsex', 'hrace', 'age', 'hyrsed']]
 df2['annhrs'] = df2['hannhrs'].where(df2['hannhrs']>0)
@@ -80,30 +46,15 @@ df2['hrwage'] = df2['hlabinc']/df2['annhrs'] # compute hourly wage
 df2 = df2[(df2.hsex == 1.0) & (df2.age >= 25) & (df2.age <= 60) & (df2.hrwage > 7)] #Part 1
 df2['ln_hrwage'] = np.log(df2['hrwage']) #log of wages
 df2.describe()
-
-
-# In[8]:
-
-
 # This is a note for me: https://stackoverflow.com/questions/11587782/creating-dummy-variables-in-pandas-for-python
 race_dummy = pd.get_dummies(df2['hrace']) # Defining race dummies (There is no Hispanic individual in dataset)
 df2['constant'] = 1
-data = pd.concat([df2, race_dummy], axis = 1) 
-data.rename(columns = {1.0: 'White', 2.0: 'Black', 3.0: 'Others'}, inplace = True) #final data "data"; ready to estimate model 
+data = pd.concat([df2, race_dummy], axis = 1)
+data.rename(columns = {1.0: 'White', 2.0: 'Black', 3.0: 'Others'}, inplace = True) #final data "data"; ready to estimate model
 data.head()
-
-
-# In[9]:
-
-
 data.dtypes
 
-
-# In[10]:
-
-
 np.save("data", data)
-
 # Separate years for estimation
 data1971 = data[data['year'] == 1971]
 np.save("data1971", data1971)
@@ -113,10 +64,6 @@ data1990 = data[data['year'] == 1990]
 np.save("data1990", data1990)
 data2000 = data[data['year'] == 2000]
 np.save("data2000", data2000)
-
-
-# In[11]:
-
 
 #------------------------------------------------------------------------------
 # ML estimation (Part 3)
@@ -130,7 +77,7 @@ Black_it, Hispanic_it, OtherRace_it = dummy variables for race = Black, Hispanic
 '''
 
 # Define my objective function
-def myLL(params, t): 
+def myLL(params, t):
     # Coeff.s
     beta0, beta1, beta2, beta3, beta4, sigma = params
     beta = np.array([beta0, beta1, beta2, beta3, beta4])
@@ -147,10 +94,6 @@ def myLL(params, t):
     y_bar = np.dot(X, beta)
     ll = (-(n/2)*np.log(2*np.pi) - (n/2)*np.log(sigma**2) - (1/(2*sigma**2))*((y-y_bar).T @ (y - y_bar)))
     return (-ll)
-
-
-# In[12]:
-
 
 # MLE; 'Nelder-Mead'
 nbeta = 5
@@ -200,11 +143,6 @@ for t in years:
     print("_____________________________________")
 #### I tried this loop so many times and attempted to troubleshoot but finally I got this error: "string indices must be integers"
 '''
-
-
-# In[13]:
-
-
 # MLE; 'L-BFGS-B'
 nbeta = 5
 beta = np.zeros(nbeta)
@@ -244,10 +182,6 @@ print("MLE coefficients: ", "year == 2000")
 print("=============================")
 print(res2000_B)
 print("_____________________________________")
-
-
-# In[18]:
-
 
 # MLE; 'SLSQP'
 nbeta = 5
@@ -289,10 +223,6 @@ print("=============================")
 print(res2000_S)
 print("_____________________________________")
 
-
-# In[19]:
-
-
 # Comparing the MLE coefficients for 3 different methods:
 print("All data; Nelder-Mead: ", res_NM.x)
 print("1971; Nelder-Mead: ", res71_NM.x)
@@ -312,12 +242,7 @@ print("1980; L-BFGS-B: ", res80_S.x)
 print("1990; L-BFGS-B: ", res90_S.x)
 print("2000; L-BFGS-B: ", res2000_S.x)
 
-
-# In[22]:
-
-
-# OLS 
-
+# OLS
 #https://lectures.quantecon.org/py/ols.html
 # to estimate each year separately
 res_OLS = statmod.OLS(endog=data['ln_hrwage'], exog=data[['constant', 'hyrsed', 'age', 'Black', 'Others']]).fit()
@@ -359,14 +284,8 @@ for t in years:
 #  ---------------------------------
 # # Interpretation (Part 4)
 #  ---------------------------------
-# To answer this question that how the returns to education change over time in these data, we can note that it is increasing. The results for OLS estimation in the full sample indicate that 1 unit increase in the years of education of males will result in a 7.8 % increase in their wages. Using data for 1971, we can see that this increase in the wage is as much as 6.7% as a result of each year of increased education for the male heads. We can also see that the percentage of increase in the wages is 6.8%, 9.8%, & 11% for 1980, 1990, and 2000, respectively, after one more year of education. 
-# 
+# To answer this question that how the returns to education change over time in these data, we can note that it is increasing. The results for OLS estimation in the full sample indicate that 1 unit increase in the years of education of males will result in a 7.8 % increase in their wages. Using data for 1971, we can see that this increase in the wage is as much as 6.7% as a result of each year of increased education for the male heads. We can also see that the percentage of increase in the wages is 6.8%, 9.8%, & 11% for 1980, 1990, and 2000, respectively, after one more year of education.
+#
 # In addition, I estimated the same linear model using the maximum likelihood model and the estimated coefficients for the education are 7.8, 6.97, 6.7, 11.8, 10.9 percent for the full sample, 1971 sample of the data, 1980, 1990 and 2000 samples, respectively, which supports the estimation of the model using OLS both in magnitude and signature. MLE method can be estimated using several optimization methods for the likelihood function. After using Nelder-Mead method and gaining aforementioned estimations, we used bounded L-BFGS-B and SLSQP methods. The former estimates the coefficients of the education as 7.8, 6.7, 6.7, 9.7, and 10.9 % for the full dataset, 1971, 1980, 1990, 2000 datasets respectively which is againg very close to the estimations of the model using OLS methodology. But the latter estimated far away coefficients for the provided samples of the data with initial values of 0.1 for all coefficients and tolerance level of 1e-15. However, after changing the initial values of the coefficients in the optimization method, I received the same estimations as the ones in OLS and other MLE optimization methods.
-# 
+#
 # **All in all, education and wages are positively correlated and increasing the years of education will lead to higher wages.**
-
-# In[ ]:
-
-
-
-
