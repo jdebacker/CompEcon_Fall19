5029aa2f099f143145a9126762bf1437e3d5dba5
devashish2019 <thakardevashish@gmail.com>
2019-10-14 16:36:48 -0400

    adding .py module

---
+++ Problemsets/PS2/calculator_Module_PS2_Thakar.py
@@ -0,0 +1,21 @@
+
+# coding: utf-8
+
+# In[6]:
+
+def sum_calc(a,b):
+    result1=a+b        
+    return(result1)
+def product_calc(a,b):
+    result2=a*b
+    return(result2)
+def sqrt_calc(a):
+    from math import sqrt
+    result3=sqrt(a)
+    return(result3)
+
+
+# In[ ]:
+
+
+
